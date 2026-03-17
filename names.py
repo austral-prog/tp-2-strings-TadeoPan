1f288cb75d2caef3c25e31ce145f96e84ee5dc0c
TadeoPan <taapan@mail.austral.edu.ar>
2026-03-17 09:57:39 -0300

    Update names.py

--- names.py
+++ names.py
@@ -2,4 +2,9 @@ def names():
     """Lee nombre y apellido, e imprime el nombre completo en distintos formatos:
     minúsculas, título, mayúsculas y con tabulador.
     """
+    name = "ada lovelace"
+    print(name.lower())
+    print(name.title())
+    print(name.upper())
+    print("\t"+ name)
     pass
