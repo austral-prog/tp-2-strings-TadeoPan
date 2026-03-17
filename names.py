a8741b0366165c4b5828262a4b33df49cb149d09
TadeoPan <taapan@mail.austral.edu.ar>
2026-03-17 10:34:38 -0300

    Update names.py

--- names.py
+++ names.py
@@ -2,11 +2,10 @@ def names():
     """Lee nombre y apellido, e imprime el nombre completo en distintos formatos:
     minúsculas, título, mayúsculas y con tabulador.
     """
-    name = input()
-    surname = input()
+    name = input("Escribe tu nombre")
+    surname = input("Escribe tu apellido")
     nombre_completo = (name + " " + surname)
     print(nombre_completo.lower())
     print(nombre_completo.title())
     print(nombre_completo.upper())
     print("\t" + nombre_completo.lower())
-    pass
